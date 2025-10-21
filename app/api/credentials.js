ddbddfa2caddcdfa9e09ae2ea630935202984d42
Osamor Chukwuka <osamorchukwuka15@gmail.com>
2025-10-21 08:44:14 +0100

    moved to production api

--- app/api/credentials.js
+++ app/api/credentials.js
@@ -1,9 +1,11 @@
-const stagingIp = 'https://blog.test/api';
+const localIp = 'https://blog.test/api';
+const productionIp = 'https://deescribebackend-production.up.railway.app/api';
+
 // const stagingIp = 'http://127.0.0.1:8000/api';
 // const apiVersion = '/v1'
 
 export const credentials = {
-    baseUrl: stagingIp,
+    baseUrl: productionIp,
     CloudinaryApiKey: '315181771479372',
     CloudinaryApiSecret: '7X9DiKoVS7t3GqcmOC-niETOKoY',
     CloudName: 'drmjq7src',
