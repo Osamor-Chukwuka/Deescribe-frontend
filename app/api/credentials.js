9a2f0f1d7985dceb54a2dfd662054f6279188b60
Osamor Chukwuka <osamorchukwuka15@gmail.com>
2025-10-21 08:55:39 +0100

    added .env

--- app/api/credentials.js
+++ app/api/credentials.js
@@ -1,13 +1,16 @@
 const localIp = 'https://blog.test/api';
 const productionIp = 'https://deescribebackend-production.up.railway.app/api';
 
+const CloudinaryApiKey = process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY
+const CloudinaryApiSecret = process.env.NEXT_PUBLIC_CLOUDINARY_SECRET_KEY
+
 // const stagingIp = 'http://127.0.0.1:8000/api';
 // const apiVersion = '/v1'
 
 export const credentials = {
     baseUrl: productionIp,
-    CloudinaryApiKey: '315181771479372',
-    CloudinaryApiSecret: '7X9DiKoVS7t3GqcmOC-niETOKoY',
+    CloudinaryApiKey: CloudinaryApiKey,
+    CloudinaryApiSecret: CloudinaryApiSecret,
     CloudName: 'drmjq7src',
     uploadPreset: 'deescribe',
     // uploadFolder: 'deescribe/posts'
