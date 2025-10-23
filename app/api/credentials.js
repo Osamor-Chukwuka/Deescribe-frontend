bc1866d8e0f5183782b9b0259a40cf909d157426
Osamor Chukwuka <osamorchukwuka15@gmail.com>
2025-10-23 09:21:35 +0100

    Changed to prod

--- app/api/credentials.js
+++ app/api/credentials.js
@@ -8,7 +8,7 @@ const CloudinaryApiSecret = process.env.NEXT_PUBLIC_CLOUDINARY_SECRET_KEY
 // const apiVersion = '/v1'
 
 export const credentials = {
-    baseUrl: localIp,
+    baseUrl: productionIp,
     CloudinaryApiKey: CloudinaryApiKey,
     CloudinaryApiSecret: CloudinaryApiSecret,
     CloudName: 'drmjq7src',
